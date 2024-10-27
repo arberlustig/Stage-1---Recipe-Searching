617a5c95eeea7d219970143912b69c319fb7f2a2
Arber <arber0947@gmail.com>
2024-10-27 23:19:52 +0100

    Logic for displaying ingredients for specified recipes

--- Stage 1 - Recipe Searching/Classes/SearchQuery.cs
+++ Stage 1 - Recipe Searching/Classes/SearchQuery.cs
@@ -1,4 +1,5 @@
-﻿using Stage_1___Recipe_Searching.Interfaces;
+﻿using System.Security.Permissions;
+using Stage_1___Recipe_Searching.Interfaces;
 using System.Text.Json;
 using System.Threading.Channels;
 
@@ -35,9 +36,18 @@ public class SearchQuery : ISearchQuery
                 case "s":
                     RecipesForContainingSpecifiedIngredients();
                     break;
+                default:
+                    throw new ArgumentOutOfRangeException(
+                        "Please insert one of the initials that you can see on the console!");
             }
         }
         catch (ArgumentNullException e)
+        {
+            Console.WriteLine(e.ParamName);
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+        catch (ArgumentOutOfRangeException e)
         {
             Console.WriteLine(e.ParamName);
             Console.ReadKey();
@@ -53,43 +63,71 @@ public class SearchQuery : ISearchQuery
 
     private void DisplayIngredientsForSpecifiedRecipe()
     {
-        //gib ein bestimmtes rezeptname ein
-        //->also readline() zuerst
-        //dann durch die liste der rezepte durchgehen
-        //und dann auf die liste innerhalb eines rezeptes zugreifen und dann die id hernehmen und die liste der ingredients benutzen
         Console.WriteLine("Here are the available recipes");
         DisplayRecipes();
         Console.WriteLine();
         Console.WriteLine("Write the name of the recipe.");
+        Console.WriteLine();
         string userInput = Console.ReadLine();
 
-        List<RecipeClass> selectedRecipe = Recipes.FindAll(recipe =>
-            recipe.Name.Equals(userInput, StringComparison.OrdinalIgnoreCase));
-        RecipeClass recipe = selectedRecipe.First();
+        List<IngredientClass> ingredientsFromSpecifiedRecipe = IngredientsFromSpecifiedRecipe(userInput);
 
-        Console.ReadKey();
+        Console.WriteLine();
+        foreach (var item in ingredientsFromSpecifiedRecipe)
+            Console.WriteLine(item.Name);
 
+    }
 
+    private List<IngredientClass> IngredientsFromSpecifiedRecipe(string? userInput)
+    {
+        List<RecipeClass> selectedRecipe = Recipes
+            .FindAll(recipe =>
+                recipe.Name
+                    .Equals(userInput, StringComparison.OrdinalIgnoreCase));
+
+        List<int> specificRecipeIds = selectedRecipe
+            .First().IngredientIds;
+
+        List<IngredientClass> ingredientsFromSpecifiedRecipe = Ingredients
+            .Where(ingredient => specificRecipeIds
+                .Contains(ingredient.Id)).ToList();
+        return ingredientsFromSpecifiedRecipe;
     }
 
     public void DisplayRecipes()
     {
-        if (!Recipes.Any())
-            Console.WriteLine("nothing");
-
-        foreach (var recipe in Recipes)
+        try
         {
-            Console.WriteLine(@$"{recipe.Id}. {recipe.Name}");
+            if (!Recipes.Any())
+                throw new ArgumentNullException("Liste ist noch leer!");
+
+            foreach (var recipe in Recipes)
+            {
+                Console.WriteLine(@$"{recipe.Id}. {recipe.Name}");
+            }
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.ParamName);
         }
     }
 
     public async Task<List<IngredientClass>> GetIngredients()
     {
 
-        var basePath = AppDomain.CurrentDomain.BaseDirectory;
-        var jsonFilePath = Path.Combine(basePath, @"..\..\..\FilesWithContentJSON\ingredients.json");
-        var jsonString = File.ReadAllText(jsonFilePath);
-        return JsonSerializer.Deserialize<List<IngredientClass>>(jsonString);
+        try
+        {
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            var jsonFilePath = Path.Combine(basePath, @"..\..\..\FilesWithContentJSON\ingredients.json");
+            var jsonString = File.ReadAllText(jsonFilePath);
+            return JsonSerializer.Deserialize<List<IngredientClass>>(jsonString);
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return null;
     }
 
     public async Task<List<RecipeClass>> GetRecipes()
